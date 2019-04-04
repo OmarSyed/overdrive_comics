ed489ba3220f9a199c03fd8211321570db1f1147
Jon M <jmat8116@gmail.com>
2019-04-04 01:27:50 -0400

    Fixing more url links

--- src/main/java/com/example/demo/controllers/TemplateController.java
+++ src/main/java/com/example/demo/controllers/TemplateController.java
@@ -21,4 +21,75 @@ public class TemplateController {
 	public String series() {
 		return "series";
 	}
+	
+	@RequestMapping("/create")
+	public String create() {
+		return "create";
+	}
+	
+	@RequestMapping("/dashboard")
+	public String dashboard() {
+		return "dashboard";
+	}
+	
+	@RequestMapping("/discover")
+	public String discover() {
+		return "discover";
+	}
+	
+	@RequestMapping("/editor")
+	public String editor() {
+		return "editor";
+	}
+	
+	@RequestMapping("/genres")
+	public String genres() {
+		return "genres";
+	}
+	
+	@RequestMapping("/profile")
+	public String profile() {
+		return "profile";
+	}
+	
+	@RequestMapping("/search")
+	public String search() {
+		return "search";
+	}
+	
+	@RequestMapping("/settings")
+	public String settings() {
+		return "settings";
+	}
+	
+	@RequestMapping("/signup")
+	public String signup() {
+		return "signup";
+	}
+	/*
+	 * Below list of css files 
+	 * that will be served
+	 */
+	@RequestMapping("/maincss")
+	public String mainCSS() {
+		return "./css/main.css";
+	}
+	
+	@RequestMapping("/literallycanvascss")
+	public String literallyCanvasCSS() {
+		return "./js/canvas/css/literallycanvas.css";
+	}
+	
+	/*
+	 * For JS files that will be served
+	 */
+	@RequestMapping("/mainjs")
+	public String mainJS() {
+		return "./js/main.js";
+	}
+	
+	@RequestMapping("/literallycanvasjs")
+	public String literallyCanvasJS() {
+		return "./js/canvas/js/literallycanvas.min.js";
+	}
 }
