fa7cca649bec1e5d0ed081b30eb4c73977a41b3c
OmarSyed <omarsyed26@gmail.com>
2019-05-19 14:49:24 -0400

    Added search endpoint and changed method for series repository

--- src/main/java/com/example/demo/controllers/ComicSeriesController.java
+++ src/main/java/com/example/demo/controllers/ComicSeriesController.java
@@ -497,27 +497,6 @@ public class ComicSeriesController {
 		return chapters;
 	}
 
-	// return popular series
-	@RequestMapping(value = "/popular/{option}", method = RequestMethod.GET)
-	public List<ComicSeries> getPopular(@PathVariable String option) {
-		System.out.println("stuff");
-		List<ComicSeries> all_popular_followers = seriesrepository.findByGenreOrderByFollowersDesc(option);
-		List<ComicSeries> all_popular_likes = seriesrepository.findByOrderByLikesDesc();
-		//Set<ComicSeries> s = new HashSet<ComicSeries>();
-		//s.addAll(all_popular_likes);
-		//s.addAll(all_popular_followers);
-		//return s;
-		//all_popular_followers.addAll(all_popular_likes);
-		//List<ComicSeries> listWithoutDuplicates = all_popular_followers.stream().distinct().collect(Collectors.toList());
-		if (all_popular_followers.size() > 20) {
-			List<ComicSeries> second = new ArrayList<ComicSeries>(all_popular_followers.subList(0, 20));
-			return second;
-		} else {
-			return all_popular_followers;
-		}
-		//return all_popular_followers;
-	}
-
 	// add comment to chapter
 	@RequestMapping(value = "chapter/addComment", method = RequestMethod.POST)
 	public Comment addComment(@Valid @RequestBody Comment comment) {
@@ -641,6 +620,26 @@ public class ComicSeriesController {
 			}
 		}
 		return suggested;
+	}
+	
+	@RequestMapping(value = "/search/{query}", method = RequestMethod.GET)
+	public List<ComicSeries> search(@PathVariable String query) {
+		List<ComicSeries> series = seriesrepository.findByComicSeriesNameIgnoreCaseLikeOrderByFollowersDesc(query);
+		List<String> ids = new ArrayList<String>();
+		
+		for(int i = 0; i<series.size(); i++) {
+			ids.add(series.get(i).getSeriesId());
+		}
+		List<ComicSeries> des = seriesrepository.findByDescriptionIgnoreCaseLikeOrderByFollowersDesc(query);
+		//series.addAll(des);
+		for(int i = 0; i<des.size(); i++) {
+			if(ids.contains(des.get(i).getSeriesId())) {
+				System.out.println("duplicate");
+			}else {
+				series.add(des.get(i));
+			}
+		}
+		return series;
 	}
 
 
